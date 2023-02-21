e596158a941e524a2644aedf8b81b6ad520bd4d1
JasicTheBeast <109973784+JasicTheBeast@users.noreply.github.com>
2023-02-20 19:53:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function startClassification()
+{
+    Navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = m15.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json', modelReady);
+        
+    }
+ 
+    function modelReady(){
+
+        classifier.classify(gotResults);
+    }
